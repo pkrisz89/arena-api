34b8af2398fa60248b83e86707e0112826db4516
Kris Pinter <pinter.krisztian89@gmail.com>
2018-11-03 09:55:04 +0000

    init

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+
+const isProd = process.env.NODE_ENV === "prod";
+const PORT = isProd ? process.env.PORT : 9000;
+
+function getCallerIP(request) {
+  let ip =
+    request.headers["x-forwarded-for"] ||
+    request.connection.remoteAddress ||
+    request.socket.remoteAddress ||
+    request.connection.socket.remoteAddress;
+
+  let port =
+    request.connection.remotePort ||
+    request.socket.remotePort ||
+    request.connection.socket.remotePort;
+
+  ip = ip.split(",")[0];
+  ip = ip.split(":").slice(-1);
+  return { ip, port };
+}
+
+app.get("/", function(req, res) {
+  const { ip, port } = getCallerIP(req);
+  res.json({ ip, port });
+});
+
+app.listen(PORT, () =>
+  console.log(`Listening to ${PORT} in ${process.env.NODE_ENV} mode`)
+);
